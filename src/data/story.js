d963259b18b4cc8c0617eecd0fd8bbf6e2c3c648
JoshiDeepak08 <joshideepak2334@gmail.com>
2026-01-09 17:42:03 +0530

    initial commit

---
+++ src/data/story.js
@@ -0,0 +1,22 @@
+export const story = [
+  {
+    image: "/src/assets/images/img1.jpg",
+    text: "Hiiii Goluuuu, Just wanted to tell you somethingggg...",
+  },
+  {
+    image: "/src/assets/images/img2.jpg",
+    text: "Do you remeber this picture, Our first Picture that you took ✨",
+  },
+  {
+    image: "/src/assets/images/img3.jpg",
+    text: "Our First trip as a hackathon... 🥺",
+  },
+  {
+    image: "/src/assets/images/img4.jpg",
+    text: "A day, full of enjoyymenttt 😌",
+  },
+  {
+    image: "/src/assets/images/img5.jpg",
+    text: "Yeah sometimes I do things that make you angryy, And I am sorry for that, But Just like this Image, I choose US, so Maaf krde goluu ❤️",
+  },
+];
